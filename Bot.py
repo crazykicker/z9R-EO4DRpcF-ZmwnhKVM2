afaa27837f1aa7770a5de647f68992f968c99858
crazykicker <60672064+crazykicker@users.noreply.github.com>
2020-11-08 05:18:15 +0300

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -1,240 +1,119 @@
-#импорт нужных бибиотек
-import os
-import datetime
-from datetime import timedelta
-import time
-from time import gmtime, strftime
-import vk_api
-import random
+import requests
+from datetime import datetime
 from vk_api import VkApi
 from vk_api.bot_longpoll import VkBotLongPoll
 from vk_api.bot_longpoll import VkBotEventType
 from vk_api.keyboard import VkKeyboard, VkKeyboardColor
 
-print(time.ctime())
-groupsession = "174863134"
-users_id = (37916557, 179099319, 166608781, 214172854, 156660737, 171561963, 409943726, 288452948, 146482440, 49920565, 158855545, 54790058, 157485694, 22440645)
+HEADERS = {"Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+           "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.183 Safari/537.36"}
 
-
-chislitel_poned = open("poned_shisl.txt").read()
-#print(chislitel_poned)
-
-chislitel_vtor = open("vtor_shils.txt").read()
-#print(chislitel_vtor)
-
-chislitel_sred = open("sred_chislit.txt").read()
-#print(chislitel_sred)
-
-chislitel_chet = open("chet_chislit.txt").read()
-#print(chislitel_chet)
-
-chislitel_pyat = open("pyat.txt").read()
-#print(chislitel_pyat)
-
-subbota = "Суббота. \n \nЗанятий не запланировано"
-voskresenye = "Воскресенье \n \nЗанятий не запланировано"
-
-
-
-znamenatel_poned = open("poned_znam.txt").read()
-
-znamenatel_vtor = open("vtor_znam.txt").read()
-
-znamenatel_sred = open("sred_znam.txt").read()
-
-znamenatel_chet = open("chet_znam.txt").read()
-
-znamenatel_pyat = open("pyat.txt").read()
-
-
-
-#подключение к сообществу вк и извлечение расписания с файла
-token = os.environ.get("bot_tocken")
+token = "49538c4d13af14cd3a35864f1a470a23430395edcdf87ad7d3f065f98c19690e95ebbae21407bd02b240a"
+groupsession = "106569786"
 vk_session = VkApi(token=token)
 longpoll = VkBotLongPoll(vk_session, groupsession)
 vk = vk_session.get_api()
-raspis_chisl = (voskresenye, chislitel_poned, chislitel_vtor, chislitel_sred, chislitel_chet, chislitel_pyat, subbota)
-raspis_znam = (voskresenye, znamenatel_poned, znamenatel_vtor, znamenatel_sred, znamenatel_chet, znamenatel_pyat, subbota)
 
-#создание начальной кнопки
-def create_welcome():
+def create_url():
+    now = datetime.now().strftime("%d" + "." + "%m" + "." + "%Y")
+    URL = "https://www.sberbank.ru/portalserver/proxy/?pipe=shortCachePipe&url=http%3A%2F%2Flocalhost%2Frates-web%2FrateService%2Frate%2Fcurrent%3FregionId%3D77%26rateCategory%3Dcards%26currencyCode%3D978%26date%3D" + now + "%2023%3A59%3A59"
+    return URL
+
+
+def get_html(url, params=""):
+    r = requests.get(url, headers=HEADERS, params=params)
+    return r
+
+def parse_course():
+    html = get_html(create_url())
+    if html.status_code == 200:
+        k = html.text.split(":")
+        z = k[11].split(",")
+        return float(z[0])
+    else:
+        print("Error")
+
+def convert_in_rub(cost):
+    cost *= parse_course()
+    cost = ("%.2f" % cost)
+    return cost
+
+def create_price(price):
+    postage = 36.0
+    price_with_vat = price * 0.83333333333
+    if price_with_vat < 200.0:
+        order_total = price_with_vat + (postage * 0.83333333333)
+        vat = order_total - (price + postage)
+        info = ("Сумма корзины: " + str(
+            convert_in_rub(price)) + "р." + "\n" + "Вычет Vat: " + str(
+            convert_in_rub(vat)) + "р." + "\n" + "Доставка: " + str(
+            convert_in_rub(postage)) + "\n" + "Общаяя сумма заказа: " + str(convert_in_rub(order_total)) + "р.")
+
+    elif price_with_vat > 200.0:
+        vat = price_with_vat - price
+        nalog = price_with_vat - 200.0
+        nalog *= 0.15
+        order_total = price_with_vat + nalog
+        info = ("Сумма корзины: " + str(
+            convert_in_rub(price)) + "р." + "\n" + "Вычет Vat: " + str(
+            convert_in_rub(vat)) + "р." + "\n" + "Налог Рф: " + str(
+            convert_in_rub(nalog)) + "р." + "\n" + "Общаяя сумма заказа: " + str(convert_in_rub(order_total)) + "р.")
+
+    return info
+
+def create_course():
+    course = "Текущий курс Сбербанка: 1 евро = " + str(parse_course()) + "р."
+    return course
+
+def create_keyboard(id, message):
     keyboard = VkKeyboard(one_time=False)
-    keyboard.add_button('Расписание', color=VkKeyboardColor.POSITIVE)
-    vk.messages.send(user_id=event.object.from_id, random_id=0, keyboard=keyboard.get_keyboard(),message="Привет друг. Возможно ты первый раз меня используешь, но здесь все просто, у тебя снизу есть кнопка, послее ее нажатия ты получишь меню с моими функциями. \n \n Совет: чтобы обновить текущую дату в меню расписания нажми выход и снова открой расписание.")
+    keyboard.add_button("Калькулятор", color=VkKeyboardColor.POSITIVE)
+    vk.messages.send(user_id=id, random_id=0, keyboard=keyboard.get_keyboard(), message=message)
 
-#создание выхода в меню
-def create_keyboard1():
+def create_calc(id):
     keyboard = VkKeyboard(one_time=False)
-    keyboard.add_button('Расписание', color=VkKeyboardColor.POSITIVE)
-    vk.messages.send(user_id=event.object.from_id, random_id=0, keyboard=keyboard.get_keyboard(),message='Ок. выбери новую команду.')
-
-#создание дат на 7 дней вперед и  кнопок с датами
-def create_keyboard2():
-    currentdate = []
-    ld = []
-    a = datetime.date.today()
-    one_day = timedelta(1)
-    now = a - one_day
-    days = {0: u"Вс", 1: u"Пн", 2: u"Вт", 3: u"Ср", 4: u"Чт", 5: u"Пт", 6: u"Сб"}
-    i = 0
-    while i < 7:
-        now = now + one_day
-        key = str(now)
-        key1 = key[8] + key[9] + " " + key[5] + key[6] + " " + key[0] + key[1] + key[2] + key[3]
-        rday = time.strptime(key1, "%d %m %Y")
-        nWeek = strftime("%w", rday)
-        date = key1
-        #DEBUG print(nWeek)
-        day = [days[int(nWeek)]]
-        inday = str(key1) + str(day)
-        #DEBUG print(inday)
-        ld.append(inday)
-        i = i + 1
-
-    count = (len(ld))
-    g = 0
-
-    while g < int(count):
-        a = ld[g]
-        b = a[0] + a[1] + "." + a[3] + a[4] + "," + a[12] + a[13]
-        currentdate.append(b)
-        g = g + 1
-    #DEBUG print(currentdate)
-    keyboard = VkKeyboard(one_time=False)
-    keyboard.add_button(currentdate[1], color=VkKeyboardColor.DEFAULT)
-    keyboard.add_button(currentdate[2], color=VkKeyboardColor.DEFAULT)
-    keyboard.add_button(currentdate[3], color=VkKeyboardColor.DEFAULT)
-    keyboard.add_line()
-    keyboard.add_button(currentdate[4], color=VkKeyboardColor.DEFAULT)
-    keyboard.add_button(currentdate[5], color=VkKeyboardColor.DEFAULT)
-    keyboard.add_button(currentdate[6], color=VkKeyboardColor.DEFAULT)
-    keyboard.add_line()
-    keyboard.add_button(currentdate[0] + " " + "-" + " " + "Сегодня", color=VkKeyboardColor.POSITIVE)
+    keyboard.add_button("Курс", color=VkKeyboardColor.SECONDARY)
     keyboard.add_button("Выход", color=VkKeyboardColor.NEGATIVE)
-    vk.messages.send(user_id=event.object.from_id, random_id=0, keyboard=keyboard.get_keyboard(),message="Выбери нужную дату.")
+    vk.messages.send(user_id=id, random_id=0, keyboard=keyboard.get_keyboard(), message="Чтобы расчитать стоимость покупки в рублях введите стоимость товара в евро.")
+    while True:
+        for event in longpoll.listen():
+            if event.type == VkBotEventType.MESSAGE_NEW:
+                text = event.object.text.lower()
+                user = event.object.from_id
+                if text == "курс":
+                    vk.messages.send(user_id=user, random_id=0, message=create_course())
 
-#проверка на весокосный год
-def year_cheak():
-    nowyear = int(datetime.date.today().strftime("%Y"))
-    if nowyear % 4 == 0:
-        if nowyear % 100 == 0:
-            if nowyear % 400 == 0:
-                date_check_vesokos(case1, case2)
-            else:
-                date_check_nevesokos(case1, case2)
-        else:
-            date_check_vesokos(case1, case2)
-    else:
-        date_check_nevesokos(case1, case2)
+                elif text == "выход":
+                    create_keyboard(user, "Выхожу....")
+                    return
 
-#проверка валидности дней для весокосного года
-def date_check_vesokos(case1, case2):
-    if case2 == 2:
-        if case1 > 29:
-            vk.messages.send(user_id=event.object.from_id, random_id=0, message='Некорректная дата')
-        else:
-            week_check(case1, case2)
-    elif case2 == 1 or case2 == 3 or case2 == 5 or case2 == 7 or case2 == 8 or case2 == 10 or case2 == 12:
-        week_check(case1, case2)
-    elif case2 == 4 or case2 == 6 or case2 == 9 or case2 == 11:
-        if case1 > 30:
-            vk.messages.send(user_id=event.object.from_id, random_id=0, message='Некорректная дата')
-        else:
-            week_check(case1, case2)
-
-#проверка валидности для обычного года
-def date_check_nevesokos(case1, case2):
-    if case2 == 2:
-        if case1 > 28:
-            vk.messages.send(user_id=event.object.from_id, random_id=0, message='Некорректная дата')
-        else:
-            week_check(case1, case2)
-    elif case2 == 1 or case2 == 3 or case2 == 5 or case2 == 7 or case2 == 8 or case2 == 10 or case2 == 12:
-        week_check(case1, case2)
-    elif case2 == 4 or case2 == 6 or case2 == 9 or case2 == 11:
-        if case1 > 30:
-            vk.messages.send(user_id=event.object.from_id, random_id=0, message='Некорректная дата')
-        else:
-            week_check(case1, case2)
-
-#проверка числителя знаменателя
-def week_check(case1, case2):
-
-    allcase = str(case1) + " " + str(case2) + " " + datetime.date.today().strftime("%Y")
-    weekcase = time.strptime(allcase, "%d %m %Y")
-    weeckcheck = int(strftime("%W", weekcase))
-    #DEBUG print(weeckcheck)
-    if weeckcheck % 2 == 0:
-        raspis_if_nedela_chisl(case1, case2, firsttext, secondtext)
-    else:
-        raspis_if_nedela_znam(case1, case2, firsttext, secondtext)
-
-#проверка дня недели для числителя
-def raspis_if_nedela_chisl(case1, case2, firsttext, secondtext):
-    datecase = str(case1) + " " + str(case2) + " " + datetime.date.today().strftime("%Y")
-    createday = time.strptime(datecase, "%d %m %Y")
-    daycheck = int(strftime("%w", createday))
-    raspisinday = "Неделя: Числитель." + "\n" + "День: " + firsttext + "." + secondtext + " " + "-" + " " + raspis_chisl[daycheck]
-    #print(raspisinday)
-    vk.messages.send(user_id=event.object.from_id, random_id=0, message=raspisinday)
-
-def raspis_if_nedela_znam(case1, case2, firsttext, secondtext):
-    datecase = str(case1) + " " + str(case2) + " " + datetime.date.today().strftime("%Y")
-    createday = time.strptime(datecase, "%d %m %Y")
-    daycheck = int(strftime("%w", createday))
-    raspisinday = "Неделя: Знаменатель." + "\n" + "День: " + firsttext + "." + secondtext + " " + "-" + " " + raspis_znam[daycheck]
-    #print(raspisinday)
-    vk.messages.send(user_id=event.object.from_id, random_id=0, message=raspisinday)
-
-#рассылка сообщений
-def send_notification(users_id):
-    u = 0
-    while u < len(users_id):
-        usrsended = users_id[u]
-        vk.messages.send(user_id=usrsended, random_id=0, message=admintext)
-        u = u + 1
-    vk.messages.send(user_id=148077344, random_id=0, message="Уведомление отправлено")
-
-#работа с longpoll api и проверка команд пользователя
-while True:
-    for event in longpoll.listen():
-        if event.type == VkBotEventType.MESSAGE_NEW:
-            usertext = event.object.text.lower()
-            admintext = event.object.text
-            #Debug print(usertext)
-            if len(usertext) < 5:
-                vk.messages.send(user_id=event.object.from_id, random_id=0, message='Некорректный ввод')
-            else:
-                firsttext = usertext[0] + usertext[1]
-                secondtext = usertext[3] + usertext[4]
-
-                if usertext[0] == ":" and int(event.object.from_id) == 148077344:
-                    admintext = "Сообщение от старосты!   " + admintext
-                    send_notification(users_id)
                 else:
+                    try:
+                        count = float(text)
+                        vk.messages.send(user_id=user, random_id=0, message=create_price(count))
+                        create_keyboard(user, "Удачного дропа!")
+                        return
+                    except:
+                        vk.messages.send(user_id=user, random_id=0,
+                                         message="Неккоректный ввод. Введите стоимость покупки.")
 
-                    if usertext.isalpha():
-                        if usertext == "начать" or usertext == "привет":
-                            create_welcome()
-                        elif usertext == "расписание":
-                            create_keyboard2()
-                        elif usertext == "выход":
-                            create_keyboard1()
-                        else:
-                            vk.messages.send(user_id=event.object.from_id, random_id=0, message='Я тебя не понял')
 
-                    elif usertext[2] == ".":
-                        if firsttext.isdigit() and secondtext.isdigit():
-                            if int(firsttext) > 31 or int(firsttext) < 1:
-                                vk.messages.send(user_id=event.object.from_id, random_id=0, message='Некорректный ввод')
-                            else:
-                                if int(usertext[3] + usertext[4]) > 12 or int(usertext[3] + usertext[4]) < 1:
-                                    vk.messages.send(user_id=event.object.from_id, random_id=0, message='Некорректный ввод')
-                                else:
-                                    case1 = int(firsttext)
-                                    case2 = int(secondtext)
-                                    year_cheak()
-                        else:
-                            vk.messages.send(user_id=event.object.from_id, random_id=0, message='Некорректный ввод')
-                    else:
-                        vk.messages.send(user_id=event.object.from_id, random_id=0, message='Некорректный ввод')
+
+
+
+
+while True:
+     for event in longpoll.listen():
+         if event.type == VkBotEventType.MESSAGE_NEW:
+             usertext = event.object.text.lower()
+             user = event.object.from_id
+             if usertext == "начать":
+                create_keyboard(user, "Привет, чтобы использовать мои функции нажми соответсвуюущую кнопку")
+
+             elif usertext == "калькулятор":
+                 create_calc(user)
+
+             else:
+                 vk.messages.send(user_id=user, random_id=0, message="Не знаю такой команды((")
+
+
