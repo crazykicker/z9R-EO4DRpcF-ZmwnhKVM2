e053944f5386a2a08e3a150c643afaade34d44fb
crazykicker <60672064+crazykicker@users.noreply.github.com>
2020-11-30 08:16:21 +0300

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -5,22 +5,23 @@ hook = Webhook("https://discord.com/api/webhooks/782830885657116672/6G7KkY2twhRA
 
 url = "https://www.adidas.ru/api/products/F36640/availability"
 url1 = "https://www.adidas.com/yeezy"
-HEADERS = {"Accept": "accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+#HEADERS = {"Accept": "accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
             "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36"}
 
 
-def get_html(url, params=""):
-    r = requests.get(url, headers=HEADERS, params=params)
-    return r
+#def get_html(url, params=""):
+   # r = requests.get(url, headers=HEADERS, params=params)
+   # return r
 
 
-while True:
-    html = get_html(url)
-    if html.status_code == 200:
-        a = html.text.split(",")
-        a = a[1].split(":")
-        hook.send("Status: " + str(a[1]))
+#while True:
+ #   html = get_html(url)
+  #  if html.status_code == 200:
+   #     a = html.text.split(",")
+    #    a = a[1].split(":")
+     #   hook.send("Status: " + str(a[1]))
 
-    else:
-        print(html.status_code)
-    time.sleep(60)
+    #else:
+     #   print(html.status_code)
+   # time.sleep(60)
+hook.send("hi")
