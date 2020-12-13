ea31487daa5b637e966c985b761392ac0df1bbdd
crazykicker <60672064+crazykicker@users.noreply.github.com>
2020-12-13 15:16:42 +0300

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -10,7 +10,7 @@ hook = Webhook(os.environ.get('BOT_HOOK'))
 api = os.environ.get('BOT_TOKEN')
 
 HEADERS = {"Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-           "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.183 Safari/537.36"}
+           "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Safari/605.1.15"}
 
 client = discord.Client()
 
